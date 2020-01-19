c9cc719bd52897436667cc1bfe7c4ad2020a9a63
Eddy <edouardo.magini@hec.ca>
2020-01-18 20:41:31 -0500

    pushed mobile app

---
+++ AIR/app/src/main/java/com/example/air/InformationActivity.java
@@ -0,0 +1,16 @@
+package com.example.air;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+
+
+
+}
