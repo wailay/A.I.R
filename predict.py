e74a4c6b31afd472382b8e37a891d682a4552074
Emile Normand <emile@normand.com>
2020-01-18 23:03:02 -0500

    model with pizza and can

---
+++ predict.py
@@ -0,0 +1,129 @@
+import torch, torchvision
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import time
+from torchsummary import summary
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from PIL import Image
+
+
+def predict(model, test_image_name):
+    '''
+    Function to predict the class of a single test image
+    Parameters
+        :param model: Model to test
+        :param test_image_name: Test image
+
+    '''
+    
+    transform = image_transforms['test']
+
+    test_image = Image.open(test_image_name)
+    plt.imshow(test_image)
+    
+    test_image_tensor = transform(test_image)
+
+    if torch.cuda.is_available():
+        test_image_tensor = test_image_tensor.view(1, 3, 224, 224).cuda()
+    else:
+        test_image_tensor = test_image_tensor.view(1, 3, 224, 224)
+    
+    with torch.no_grad():
+        model.eval()
+        # Model outputs log probabilities
+        out = model(test_image_tensor)
+        
+        ps = torch.exp(out)
+        topk, topclass = ps.topk(nb_prediction, dim=1)
+        for i in range(nb_prediction):
+            print("Prediction", i+1, ":", idx_to_class[topclass.cpu().numpy()[0][i]], ", Score: ", topk.cpu().numpy()[0][i])
+
+def computeTestSetAccuracy(model, loss_criterion):
+    '''
+    Function to compute the accuracy on the test set
+    Parameters
+        :param model: Model to test
+        :param loss_criterion: Loss Criterion to minimize
+    '''
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    test_acc = 0.0
+    test_loss = 0.0
+
+    # Validation - No gradient tracking needed
+    with torch.no_grad():
+
+        # Set to evaluation mode
+        model.eval()
+
+        # Validation loop
+        for j, (inputs, labels) in enumerate(test_data_loader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Forward pass - compute outputs on input data using the model
+            outputs = model(inputs)
+
+            # Compute loss
+            loss = loss_criterion(outputs, labels)
+
+            # Compute the total loss for the batch and add it to valid_loss
+            test_loss += loss.item() * inputs.size(0)
+
+            # Calculate validation accuracy
+            ret, predictions = torch.max(outputs.data, 1)
+            correct_counts = predictions.eq(labels.data.view_as(predictions))
+
+            # Convert correct_counts to float and then compute the mean
+            acc = torch.mean(correct_counts.type(torch.FloatTensor))
+
+            # Compute total accuracy in the whole batch and add to valid_acc
+            test_acc += acc.item() * inputs.size(0)
+
+            print("Test Batch number: {:03d}, Test: Loss: {:.4f}, Accuracy: {:.4f}".format(j, loss.item(), acc.item()))
+
+    # Find average test loss and test accuracy
+    avg_test_loss = test_loss/test_data_size 
+    avg_test_acc = test_acc/test_data_size
+
+    print("Test accuracy : " + str(avg_test_acc))
+
+
+nb_prediction = 2
+model = torch.load('PIZZA_CAN_model_9.pt')
+
+#Applying Transforms to the Data
+image_transforms = { 
+    'test': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+}
+dataset = 'PIZZA_CAN'
+bs=10
+test_directory = os.path.join(dataset, 'test')
+data = {
+    'test': datasets.ImageFolder(root=test_directory, transform=image_transforms['test'])
+}
+
+
+idx_to_class = {v: k for k, v in data['test'].class_to_idx.items()}
+predict(model, 'PIZZA_CAN/test/pizza_box/198.greenbox_pizza.jpg')
+
+
+# loss_func = nn.NLLLoss()
+
+#test_data_size = len(data['test'])
+#test_data_loader = torch.utils.data.DataLoader(data['test'], batch_size=bs, shuffle=True)
+
+# computeTestSetAccuracy(model, loss_func)
